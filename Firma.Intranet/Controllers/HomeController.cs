47f3160de700e7a3454668034077c2845ac62031
Karol Młynek <karol.mlynek@microsoft.wsei.edu.pl>
2025-05-18 01:52:05 +0200

    Home Page Intranet edit with actual data

--- Firma.Intranet/Controllers/HomeController.cs
+++ Firma.Intranet/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using Firma.Data.Data;
 using Firma.Intranet.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace Firma.Intranet.Controllers
@@ -7,27 +9,22 @@ namespace Firma.Intranet.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly FirmaContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, FirmaContext context)
         {
             _logger = logger;
+            _context = context;
         }
-   
+
         public IActionResult Index()
         {
-            return View();
-        }
-        public IActionResult Jachty()
-        {
-            return View();
-        }
-        public IActionResult Strony()
-        {
-            return View();
-        }
-   
-        public IActionResult Zapytania()
-        {
+            // Ustawienie liczb do wyœwietlenia w panelu
+            ViewBag.LiczbaStron = _context.Strona.Count();          
+            ViewBag.LiczbaZapytan = _context.Zapytania.Count();     
+            ViewBag.LiczbaRezerwacji = _context.Rezerwacje.Count(); 
+            ViewBag.LiczbaKlientow = _context.Klienci.Count();        
+
             return View();
         }
 
